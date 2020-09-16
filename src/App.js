5c47aae2b2902317bde5a375f537071b06ceb124
madhumithaa <madhumithaa1411@gmail.com>
2020-09-16 11:52:41 +0530

    api get request done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 export default class App extends Component {
+  state = {
+    results: [],
+  };
+  componentDidMount() {
+    alert("hello");
+    axios({
+      method: "get",
+      url: "https://gprs-api.geopits.com/workflow/invoices/",
+      // data: {
+      //   firstName: "Fred",
+      //   lastName: "Flintstone",
+      // },
+      headers: { Authorization: "sessionid i6vegvvlgoxagv5mdaarhudj42wrrrsp" },
+    })
+      .then((response) => {
+        this.setState({
+          ...this.state,
+          results: response.data.results,
+        });
+        console.log(this.state.results);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>
-        <h1>hello</h1>
+        {this.state.results.map((result) => (
+          <p>
+            Hello, {result.name} from {result.slug}!
+          </p>
+        ))}
       </div>
     );
   }
